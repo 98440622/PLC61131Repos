19d0510d9f4ff41c7608d5ff45d3c238e1bd3304
98440622 <98440622@163.com>
2021-03-08 21:08:10 +0800

    Created an awesome test agent. If it works well, it will be added to my toolkit box. We'll see...

--- BZ172Lab/BZ172Lab.py
+++ BZ172Lab/BZ172Lab.py
@@ -20,6 +20,7 @@ Version MY_VERSION
 2021.03.05 Add 'reset' interface
 2021.03.06 Add 'createbzAisleACUnit_py' to create application class for data
            point quick access
+2021.03.08 Built a test agent class and it should be awesome
 
 """
 
@@ -29,7 +30,7 @@ import os
 from datetime import datetime
 import time
 
-MY_VERSION = '21MAR03'
+MY_VERSION = '21MAR04'
 
 def createDDF(variable_filename, e2prom_filename) :
     '''
@@ -335,6 +336,14 @@ def createbzAisleACUnit_py(ddf) :
             rtn, val = self.caller.getVar($ID$)
             return [rtn, $TYPE$(val)][rtn == 0]
         else:
+            pass
+"""
+    strVARWFunc = """
+    def $DATAPOINT$(self, *args):
+        if (args == ()):
+            rtn, val = self.caller.getVar($ID$)
+            return [rtn, $TYPE$(val)][rtn == 0]
+        else:
             rtn = self.caller.setVar($ID$, args[0])
             return rtn
 """
@@ -366,7 +375,11 @@ class bzAisleACUnit :
             if (ddf.Type.iloc[i] == 'String') :
                 strLn = strSTRFunc
             else:
-                strLn = strVARFunc
+                if (ddf.RW.iloc[i] == 'R'):
+                    strLn = strVARFunc
+                else:
+                    strLn = strVARWFunc
+
                 if (ddf.Unit.iloc[i].find('.1') >= 0 or
                     ddf.Unit.iloc[i].find('.01') >= 0):
                     strLn = strLn.replace('$TYPE$', 'float')
@@ -447,29 +460,162 @@ class IrACdx36LibCaller :
             res.append(self.getVar(id))
         return res
 
+    def reset(self):
+        return self.caller.reset()
+
 # %% bzAisleACUnit test cases
-"""
-TODO : Add my test cases
-Useage :
-
-    mytest = bzAisleACUnitTestAgent()
-    mytest.testcase1()
-    ...
-    mytest.testcase2()
-
-"""
 from bzAisleACUnit import *
+import threading
+import heapq
+from queue import Queue
+import time
+from datetime import datetime
+
+class PriorityQueue:
+    def __init__(self):
+        self._queue = []
+        self._count = 0
+        self._cv = threading.Condition()
+
+    def put(self, item, priority):
+        with self._cv:
+            heapq.heappush(self._queue, (-priority, self._count, item))
+            self._count += 1
+            self._cv.notify()
+
+    def get(self):
+        with self._cv:
+            while len(self._queue) == 0:
+                self._cv.wait()
+            return heapq.heappop(self._queue)[-1]
 
 class bzAisleACUnitTestAgent:
-    def __init__(self):
-        self.unit = bzAisleACUnit()
+    """
+    Useage :
 
-    def checkFMVersion(self):
-        print(self.unit.BIOSVersion())
-        print(self.unit.AppVersion())
+        # create the unit (connect to data point agent)
+        unit = bzAisleACUnit()
 
-    def checkAmibentTemperatures(self):
-        print(self.unit.UpperReturnAirTemperature())
+        # create test agent
+        tester = bzAisleACUnitTestAgent(unit)
+
+        # start a background thread to process periodical requests and user reqs
+        mytester.start()
+
+        # poll a data point
+        mytester.execute(unit.UpperReturnAirTemperature)
+
+        # update a data point
+        mytester.execute(unit.UnitModeCfg, 1)
+
+        # call a predefined bunch of commands
+        mytester.getFMVersion()
+
+        ...
+
+        # stop the test agent
+        mytest.stop()
+
+    """
+    def __init__(self, unit):
+        self.unit = unit
+        self.is_started = False
+        self.req_q = Queue()
+        self.rpl_q = Queue()
+
+    def __run_at_1Hz(self):
+        dt = datetime.now()
+        dt_fmt = "{0:02d}-{1:02d} {2:02d}:{3:02d}:{4:02d}"
+        dt_str = "{0}{1:02d}{2:02d}_{3:02d}{4:02d}{5:02d}".format(dt.year,
+                                                                  dt.month,
+                                                                  dt.day,
+                                                                  dt.hour,
+                                                                  dt.minute,
+                                                                  dt.second)
+        with open("bzAisleACUnitTestLog_{0}.log".format(dt_str), "w") as file:
+            print("tester background task is started")
+            file.write('DateTime,\n')
+            while (self.is_started):
+                while (not self.req_q.empty()):
+                    obj = self.req_q.get_nowait()
+                    if (len(obj) == 1):
+                        # read
+                        self.rpl_q.put(obj[0]())
+                    elif (len(obj) == 2):
+                        self.rpl_q.put(obj[0](obj[1]))
+                    else:
+                        pass
+                dt = datetime.now()
+                strLn = dt_fmt.format(dt.month,
+                                      dt.day,
+                                      dt.hour,
+                                      dt.minute,
+                                      dt.second)
+                strLn += ',\n'
+                # todo : add periodical data log...
+                file.write(strLn)
+                time.sleep(1) # 1 Hz
+
+            file.close()
+            print('tester background task is done')
+
+    def start(self):
+        if (not self.is_started):
+            self.is_started = True
+            self.task = threading.Thread(name='__run_at_1Hz',
+                                         target=self.__run_at_1Hz)
+            self.task.start()
+        else:
+            print('agent is started, stop first and start again')
+
+    def stop(self):
+        self.is_started = False
+        self.task.join()
+
+    def execute(self, obj, *args):
+        if (self.is_started):
+            if (args == ()):
+                self.req_q.put([obj])
+            else:
+                self.req_q.put([obj, args[0]])
+
+            print(self.rpl_q.get())
+        else:
+            print('call start() firstly')
+
+    def getFMVersion(self):
+        self.req_q.put([self.unit.BIOSVersion])
+        self.req_q.put([self.unit.AppVersion])
+        res = []
+        while (not self.rpl_q.empty):
+            res.append(self.rpl_q.get())
+        return res
+
+    def setUnit(self, mode):
+        is_cmd = True
+        if (mode == 'off'):
+            self.req_q.put([self.unit.UnitModeCfg, 0])
+        elif (mode == 'on'):
+            self.req_q.put([self.unit.UnitModeCfg, 1])
+        elif (mode == 'man'):
+            self.req_q.put([self.unit.UnitModeCfg, 2])
+        else:
+            is_cmd = False
+            print(mode, ' is not a valid command')
+
+        if (is_cmd):
+            print(self.rpl_q.get())
+        else:
+            pass
+
+    # def getAmibentTemperatures(self):
+    #     return [self.unit.AverageReturnAirTemperature(),
+    #             self.unit.AverageSupplyAirTemperature(),
+    #             self.unit.RackInletAirTemperatureAlfa(),
+    #             self.unit.RackInletAirTemperatureBravo(),
+    #             self.unit.MaxRackInletAirTemperature()]
+
+
 
 # %% (OBSOLETE) Solo is what i want
 class BZ172Lab :
